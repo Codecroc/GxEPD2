f04f93a6f2fd9334472717a1ab4f7fe4ebbf272d
Philipp Köhler <p.koehler@codecroc.de>
2023-02-16 09:52:43 +0100

    add missing virtual functions

--- src/ist7136/GxEPD2_ist7136_COG.cpp
+++ src/ist7136/GxEPD2_ist7136_COG.cpp
@@ -6,15 +6,19 @@ GxEPD2_ist7136_COG::GxEPD2_ist7136_COG(int16_t cs, int16_t dc, int16_t rst, int1
 
 void GxEPD2_ist7136_COG::clearScreen(uint8_t value)
 {
-    writeScreenBuffer(0x10);
-    writeScreenBuffer(0x11, 0x00);
+    writeScreenBuffer();
     refresh();
-    writeScreenBuffer(0x10);
-    writeScreenBuffer(0x11, 0x00);
+    writeScreenBuffer();
 }
 
-void GxEPD2_ist7136_COG::writeScreenBuffer(uint8_t reg, uint8_t value)
+void GxEPD2_ist7136_COG::writeScreenBuffer(uint8_t value)
 {   
+   _WriteScreenBufferHelper(0x10, value);
+   _WriteScreenBufferHelper(0x11, 0x00);
+}
+
+void GxEPD2_ist7136_COG::_WriteScreenBufferHelper(uint8_t reg, uint8_t value)
+{
     _writeCommand(reg);
     _startTransfer();
     for (uint32_t i = 0; i < uint32_t(WIDTH) * uint32_t(HEIGHT) / 8; i++)
@@ -69,7 +73,8 @@ void GxEPD2_ist7136_COG::writeImage(const uint8_t bitmap[], int16_t x, int16_t y
     }
     _endTransfer();
     delay(1);            // yield() to avoid WDT on ESP8266 and ESP32
-    writeScreenBuffer(0x11, 0x00);
+    _WriteScreenBufferHelper(0x11, 0x00);
+
 }
 
 void GxEPD2_ist7136_COG::writeImage(const uint8_t *black, const uint8_t *color, int16_t x, int16_t y, int16_t w, int16_t h, bool invert, bool mirror_y, bool pgm)
@@ -93,11 +98,16 @@ void GxEPD2_ist7136_COG::get_busy_status(uint8_t busy)
     busy = digitalRead(_busy);
 }
 
-void GxEPD2_ist7136_COG::refresh()
+void GxEPD2_ist7136_COG::refresh(bool partial_update_mode)
 {
     _Init_Full();
     _waitWhileBusy("refresh", _busy_timeout);
-    _write_command_data(0x15, 0x3c);
+    _WriteCommandData(0x15, 0x3c);
+}
+
+void GxEPD2_ist7136_COG::refresh(int16_t x, int16_t y, int16_t w, int16_t h)
+{
+    refresh();
 }
 
 void GxEPD2_ist7136_COG::powerOff(void)
@@ -125,9 +135,9 @@ void GxEPD2_ist7136_COG::_PowerOff()
     if (_power_is_on)
     {
         _waitWhileBusy("_PowerOff", power_off_time);
-        _write_command_data(0x09,0x7f);
-        _write_command_data(0x05,0x7d);
-        _write_command_data(0x09,0x00);
+        _WriteCommandData(0x09,0x7f);
+        _WriteCommandData(0x05,0x7d);
+        _WriteCommandData(0x09,0x00);
         _waitWhileBusy("_PowerOff", power_off_time);
         digitalWrite(_dc, LOW);
         digitalWrite(_cs, LOW);
@@ -142,98 +152,98 @@ void GxEPD2_ist7136_COG::_InitDisplay()
         _reset();
 
     // Initial COG
-    _write_command_data(0x05, 0x7d);
+    _WriteCommandData(0x05, 0x7d);
     delay(200);
-    _write_command_data(0x05, 0x00);
+    _WriteCommandData(0x05, 0x00);
     delay(10);
     //_write_command_data(0xc2, 0x3f); // values from original library maby needs to be enabled
-    _write_command_data(0xd8, 0x00);
-    _write_command_data(0xd6, 0x00);
-    _write_command_data(0xa7, 0x10);
+    _WriteCommandData(0xd8, 0x00);
+    _WriteCommandData(0xd6, 0x00);
+    _WriteCommandData(0xa7, 0x10);
     delay(100);
-    _write_command_data(0xa7, 0x00);
+    _WriteCommandData(0xa7, 0x00);
     delay(100);
-    _write_command_data(0x44, 0x00);
-    _write_command_data(0x45,0x80);
-    _write_command_data(0xa7, 0x10);
+    _WriteCommandData(0x44, 0x00);
+    _WriteCommandData(0x45,0x80);
+    _WriteCommandData(0xa7, 0x10);
     delay(100);
-    _write_command_data(0xa7, 0x00);
+    _WriteCommandData(0xa7, 0x00);
     delay(100);
-    _write_command_data(0x44, 0x06);
-    _write_command_data(0x45, 0x80);
-    _write_command_data(0xa7, 0x10);
+    _WriteCommandData(0x44, 0x06);
+    _WriteCommandData(0x45, 0x80);
+    _WriteCommandData(0xa7, 0x10);
     delay(100);
-    _write_command_data(0xa7, 0x00);
+    _WriteCommandData(0xa7, 0x00);
     delay(100);
-    _write_command_data(0x60, 0x25);
-    _write_command_data(0x61, 0x00);
-    _write_command_data(0x02, 0x00);
+    _WriteCommandData(0x60, 0x25);
+    _WriteCommandData(0x61, 0x00);
+    _WriteCommandData(0x02, 0x00);
 }
 
-void GxEPD2_ist7136_COG::_write_command_data(uint8_t command, uint8_t data)
+void GxEPD2_ist7136_COG::_WriteCommandData(uint8_t command, uint8_t data)
 {
     _writeCommand(command);
     _writeData(data);
 }
 
-void GxEPD2_ist7136_COG::_write_command_data(uint8_t command, uint8_t * data, size_t size)
+void GxEPD2_ist7136_COG::_WriteCommandData(uint8_t command, uint8_t * data, size_t size)
 {
     _writeCommand(command);
     _writeData(data, size);
 }
 
-void GxEPD2_ist7136_COG::_soft_start_DCDC()
+void GxEPD2_ist7136_COG::_SoftStartDCDC()
 {
     uint8_t reg51[] = {0x50, 0x01, 0x0a, 0x01};
     uint8_t reg09[] = {0x1f, 0x9f, 0x7f, 0xff};
-    _write_command_data(0x51, &reg51[1], 2);
+    _WriteCommandData(0x51, &reg51[1], 2);
 
     //First Stage
     for(int i = 1; i <= 4; i++)
     {
-        _write_command_data(0x09, reg09[0]);
+        _WriteCommandData(0x09, reg09[0]);
         reg51[1] = i;
-        _write_command_data(0x51, &reg51[0], 2);
-        _write_command_data(0x09, reg09[1]);
+        _WriteCommandData(0x51, &reg51[0], 2);
+        _WriteCommandData(0x09, reg09[1]);
         delay(2);
     }
 
     //Second Stage
     for(int i = 1; i <= 10; i++)
     {
-        _write_command_data(0x09, reg09[0]);
+        _WriteCommandData(0x09, reg09[0]);
         reg51[3] = i;
-        _write_command_data(0x51, &reg51[2], 2);
-        _write_command_data(0x09, reg09[1]);
+        _WriteCommandData(0x51, &reg51[2], 2);
+        _WriteCommandData(0x09, reg09[1]);
         delay(2);
     }
 
     //Third Stage
     for(int i = 3; i <= 10; i++)
     {
-        _write_command_data(0x09, reg09[2]);
+        _WriteCommandData(0x09, reg09[2]);
         reg51[3] = i;
-        _write_command_data(0x51, &reg51[2], 2);
-        _write_command_data(0x09, reg09[3]);
+        _WriteCommandData(0x51, &reg51[2], 2);
+        _WriteCommandData(0x09, reg09[3]);
         delay(2);
     }
     // Fourth Stage 
     for(int i = 9; i >= 2; i--)
     {
-        _write_command_data(0x09, reg09[2]);
+        _WriteCommandData(0x09, reg09[2]);
         reg51[2] = i;
-        _write_command_data(0x51, &reg51[2], 2);
-        _write_command_data(0x09, reg09[3]);
+        _WriteCommandData(0x51, &reg51[2], 2);
+        _WriteCommandData(0x09, reg09[3]);
         delay(2);
     }
-    _write_command_data(0x09, reg09[3]);
+    _WriteCommandData(0x09, reg09[3]);
     delay(10);
 }
 
 void GxEPD2_ist7136_COG::_Init_Full()
 {
     _InitDisplay();
-    _soft_start_DCDC();
+    _SoftStartDCDC();
     _PowerOn();
     _using_partial_mode = false;
 }
--- src/ist7136/GxEPD2_ist7136_COG.h
+++ src/ist7136/GxEPD2_ist7136_COG.h
@@ -23,7 +23,7 @@ class GxEPD2_ist7136_COG : public GxEPD2_EPD
     // methods (virtual)
     //  Support for Bitmaps (Sprites) to Controller Buffer and to Screen
     void clearScreen(uint8_t value = 0xFF); // init controller memory and screen (default white)
-    void writeScreenBuffer(uint8_t reg, uint8_t value = 0xFF); // init controller memory (default white)
+    void writeScreenBuffer(uint8_t value = 0xFF); // init controller memory (default white)
     // write to controller memory, without screen refresh; x and w should be multiple of 8
     void writeImage(const uint8_t bitmap[], int16_t x, int16_t y, int16_t w, int16_t h, bool invert = false, bool mirror_y = false, bool pgm = false);
     void writeImagePart(const uint8_t bitmap[], int16_t x_part, int16_t y_part, int16_t w_bitmap, int16_t h_bitmap,
@@ -47,7 +47,8 @@ class GxEPD2_ist7136_COG : public GxEPD2_EPD
                        int16_t x, int16_t y, int16_t w, int16_t h, bool invert = false, bool mirror_y = false, bool pgm = false){};
     // write sprite of native data to controller memory, with screen refresh; x and w should be multiple of 8
     void drawNative(const uint8_t* data1, const uint8_t* data2, int16_t x, int16_t y, int16_t w, int16_t h, bool invert = false, bool mirror_y = false, bool pgm = false){};
-    void refresh();
+    void refresh(bool partial_update_mode = false);
+    void refresh(int16_t x, int16_t y, int16_t w, int16_t h);
     void powerOff(); // turns off generation of panel driving voltages, avoids screen fading over time
     void hibernate(); // turns powerOff() and sets controller to deep sleep for minimum power use, ONLY if wakeable by RST (rst >= 0)
     void get_busy_status(uint8_t busy);
@@ -57,9 +58,10 @@ class GxEPD2_ist7136_COG : public GxEPD2_EPD
     void _InitDisplay();
     void _Init_Full();
     void _reset();
-    void _soft_start_DCDC();
-    void _write_command_data(uint8_t command, uint8_t data);
-    void _write_command_data(uint8_t command, uint8_t *data, size_t size);
+    void _SoftStartDCDC();
+    void _WriteCommandData(uint8_t command, uint8_t data);
+    void _WriteCommandData(uint8_t command, uint8_t *data, size_t size);
+    void _WriteScreenBufferHelper(uint8_t reg, uint8_t value);
   private:
     static const unsigned char lut_20_LUTC_partial[];
     static const unsigned char lut_21_LUTWW_partial[];
